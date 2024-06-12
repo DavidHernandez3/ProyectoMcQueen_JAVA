b54d5df90b91c3d27a0730a88b22cf8a576bc28d
Jeffrey Mardoqueo <jeffreymardoqueo260@gmail.com>
2024-06-12 10:32:04 -0600

    Mejoramiento del Formulario 👩‍💻👩‍💻👩‍💻👩‍💻👩‍💻

--- src/main/java/interfazgrafica/RegistroAutos.java
+++ src/main/java/interfazgrafica/RegistroAutos.java
@@ -4,4 +4,27 @@ import javax.swing.*;
 
 public class RegistroAutos {
     private JPanel jPanel;
+    private JTextField txtId;
+    private JButton salirButton;
+    private JButton registrarAutoButton;
+    private JButton cancelarButton;
+    private JButton eliminarButton;
+    private JTextField txtBuscar;
+    private JRadioButton placaRadioButton;
+    private JButton BUSCARButton;
+    private JTextField txtPlaca;
+    private JTextField txtNombre;
+    private JTextField txtModelo;
+    private JTextField txtAnio;
+    private JTextField txtColor;
+    private JTextField txtPropietario;
+    private JButton EditarBtn;
+    private JRadioButton NombreRadioButton;
+    private JRadioButton ModeloRadioButton;
+    private JRadioButton AnioRadioButton;
+    private JRadioButton PropietarioRadioButton;
+
+    public static void main(String[] args) {
+        new RegistroAutos();
+    }
 }
